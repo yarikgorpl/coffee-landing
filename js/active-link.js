6de4ec020b85bf088470e1a01b988adcfccbf31d
Yaroslav Protsiuk <jaroslav.procjuk@gmail.com>
2025-11-19 16:25:04 +0200

    Add animation to the site
    Add transition and animation. Finish menu page.

---
+++ js/active-link.js
@@ -0,0 +1,12 @@
+function setActiveMenuLink() {
+  const hash = window.location.hash;
+  document.querySelectorAll('.menu-link').forEach(link => {
+    link.classList.toggle('active', link.getAttribute('href') === hash);
+  });
+}
+
+// При завантаженні сторінки
+window.addEventListener('DOMContentLoaded', setActiveMenuLink);
+
+// При зміні хешу (переході по якірному посиланню)
+window.addEventListener('hashchange', setActiveMenuLink);
