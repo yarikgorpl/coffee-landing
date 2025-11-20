7e07665d285e3f925f50ef7772f7cd9d6bf8c8d3
Yaroslav Protsiuk <jaroslav.procjuk@gmail.com>
2025-11-20 22:38:19 +0200

    Finish form in the modal window
    Add modal window and functional for the close button at header

--- js/modal.js
+++ js/modal.js
@@ -1,37 +1,50 @@
 (() => {
   const refs = {
-    openModalBtns: document.querySelectorAll("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    backdrop: document.querySelector("[data-backdrop]"),
+    openModalBtns: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelectorAll('[data-modal-close]'),
+    backdrop: document.querySelector('[data-backdrop]'),
+    slogan: document.querySelector('[data-slogan]'),
+    closeSloganBtn: document.querySelector('[data-slogan-close]'),
   };
 
   // Відкриття модального вікна
   refs.openModalBtns.forEach(btn => {
-    btn.addEventListener("click", openModal);
+    btn.addEventListener('click', openModal);
   });
 
   // Закриття по кнопці
-  refs.closeModalBtn.addEventListener("click", closeModal);
+  refs.closeModalBtn.forEach(btn => {
+    btn.addEventListener('click', closeModal);
+  });
+  refs.closeSloganBtn.addEventListener('click', event => {
+    if (event.currentTarget === refs.closeSloganBtn) {
+      closeSlogan();
+    }
+  });
 
   // Закриття по кліку на оверлей
-  refs.backdrop.addEventListener("click", event => {
+  refs.backdrop.addEventListener('click', event => {
     if (event.target === refs.backdrop) {
       closeModal();
     }
   });
 
   // Закриття по Escape
-  window.addEventListener("keydown", event => {
-    if (event.key === "Escape" && !refs.backdrop.classList.contains("is-hidden")) {
+  window.addEventListener('keydown', event => {
+    if (event.key === 'Escape' && !refs.backdrop.classList.contains('is-hidden')) {
       closeModal();
     }
   });
 
   function openModal() {
-    refs.backdrop.classList.remove("is-hidden");
+    refs.backdrop.classList.remove('is-hidden');
   }
 
   function closeModal() {
-    refs.backdrop.classList.add("is-hidden");
+    refs.backdrop.classList.add('is-hidden');
+  }
+
+  function closeSlogan() {
+    refs.slogan.classList.add('is-hidden');
   }
 })();
